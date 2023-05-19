5f9fad24768d746e32a110106208f9420fdf0c1e
Sakha Ardiansyah <sakhaardiansyah271202@gmail.com>
2023-05-19 17:42:54 +0700

    iinitialize

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = "Sakha Ardiansyah"; // TODO: replace this
+
+const studentId = "FE5319718"; // TODO: replace this
+
+const NetlifyDeployUrl = ""; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
